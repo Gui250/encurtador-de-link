c0378c229e6a373e7d5e03f48f11f7bde9b15135
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-09-13 18:42:59 -0300

    commit inicial

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => { 
+    return res.json("tudo certo")
+})
+
+
+app.listen(process.env.PORT, () => { 
+    console.log('Servidor Inicializado');
+});
