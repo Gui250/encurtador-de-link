97f034a8e9bd1d10c04924cdba24ccbacb434118
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-09-17 14:22:11 -0300

    alterações feitas

---
+++ src/rotas.ts
@@ -0,0 +1,11 @@
+import {  Router } from 'express';
+import CadastroLink from './controladores/CadastroLink';
+import ObterLink from './controladores/ObterLinkControlador';
+
+
+const rotas = Router(); 
+
+rotas.post('/', new CadastroLink().controlador);
+rotas.get('/:identificador', new ObterLink().controlador);
+
+export default rotas
